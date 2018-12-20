bfabc886460f9302458c25aecec7eaa209970c55
Matthew Collins <matthew.collins1991@yahoo.com>
2018-12-20 23:51:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,5 +11,5 @@ end
 def assign_rooms(speakers)
   assigned_room = []
     speakers.each_with_index   {|speaker,index| assigned_room << "Hello, #{speaker}! You'll be assigned to room #{index+1}!"}
-assigned_room
+puts assigned_room
 end
