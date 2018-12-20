a12e0f21b317a5c9bd7b90570c3a20ef414f077c
Matthew Collins <matthew.collins1991@yahoo.com>
2018-12-20 23:32:20 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    array.collect { |x| badge_maker(x)} 
+end
+
+def assign_rooms(speakers)
+  speakers.each_with_index do  |y| puts "Hello, #{speakers}! You'll be assigned to room #{speakers.index}!"
+end
+end
